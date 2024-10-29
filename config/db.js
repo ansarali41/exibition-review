1d130f02a78ebe32cc26599908662722a1896b00
Ansar Ali <ansar@amharctech.com>
2024-10-29 15:59:01 +0600

    mongo uri log

--- config/db.js
+++ config/db.js
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
+        console.log('MongoDB URI:', process.env.MONGO_URI);
+
         if (!process.env.MONGO_URI) {
             throw new Error('MONGO_URI is not defined in environment variables');
         }
